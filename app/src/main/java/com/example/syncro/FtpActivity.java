75f73b9dd9194304789824fa8b7ba3f566800756
Luis Miguel <luibelzan@outlook.com>
2026-04-09 13:24:00 +0200

    Config FTP View added

--- app/src/main/java/com/example/syncro/FtpActivity.java
+++ app/src/main/java/com/example/syncro/FtpActivity.java
@@ -1,6 +1,10 @@
 package com.example.syncro;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -19,6 +23,22 @@ public class FtpActivity extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+
+        // Spinner de protocolo
+        Spinner spinnerProtocolo = findViewById(R.id.spinnerProtocolo);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(
+                this,
+                R.array.protocolo_array,
+                android.R.layout.simple_spinner_item
+        );
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinnerProtocolo.setAdapter(adapter);
+        spinnerProtocolo.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {}
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {}
         });
     }
 }
