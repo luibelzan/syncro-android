c14db64dcaf1d220f6c7764b6e24b28d892320a6
Luis Miguel <luibelzan@outlook.com>
2026-06-19 14:03:24 +0200

    Release 1.0.4

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,7 +11,7 @@ android {
         applicationId = "com.celnet.syncro"
         minSdk = 30
         targetSdk = 36
-        versionCode = 2
+        versionCode = 3
         versionName = "1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
